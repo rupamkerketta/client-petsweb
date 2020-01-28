4d0a0270b027ebdd392abd5613e61072f42be74e
Rupam Kerketta <kerketarudolf@gmail.com>
2020-01-28 10:38:01 +0530

    Cats API fix

--- src/components/navigation/user/modal/modal.js
+++ src/components/navigation/user/modal/modal.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './modal.css';
 
+import closeBtn from '../../../res/images/close.png';
+
 const Modal = () => {
     return(
         <div className="modal">
-
+            <div className="close-btn">
+                <img src={closeBtn} alt="close"/>
+            </div>
         </div>
     );
 };

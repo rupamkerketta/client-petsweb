99302179c29540d30067e38ee17003e36e4f58cd
Rupam Kerketta <kerketarudolf@gmail.com>
2020-02-24 09:40:29 +0530

    Rescue Mod (A)

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -10,6 +10,7 @@ import Home from './home-page/homePage';
 import Dogs from './dogs/dogs';
 import Cats from './cats/cats';
 import User from './user/user';
+import Rescue from './rescue/rescue';
 
 import Logout from './logout/logout';
 
@@ -135,20 +136,20 @@ const Navigation = (props) => {
 	);
 };
 
-const Rescue = () => {
-	const style = {
-		fontFamily: 'Poppins',
-		fontSize: '2em',
-		position: 'absolute',
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%, -50%)'
-	};
-	return (
-		<div className="rescue-module" style={{ height: '50vh' }}>
-			<h2 style={style}>Rescue</h2>
-		</div>
-	);
-};
+// const Rescue = () => {
+// 	const style = {
+// 		fontFamily: 'Poppins',
+// 		fontSize: '2em',
+// 		position: 'absolute',
+// 		top: '50%',
+// 		left: '50%',
+// 		transform: 'translate(-50%, -50%)'
+// 	};
+// 	return (
+// 		<div className="rescue-module" style={{ height: '50vh' }}>
+// 			<h2 style={style}>Rescue</h2>
+// 		</div>
+// 	);
+// };
 
 export default Navigation;

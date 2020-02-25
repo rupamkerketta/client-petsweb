fdf4bfe6cdcbda48dba358d5d65ce220c2859f12
Rupam Kerketta <kerketarudolf@gmail.com>
2020-02-25 15:01:53 +0530

    Rescue Module

--- src/components/navigation/rescue/rescue.js
+++ src/components/navigation/rescue/rescue.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './rescue.css';
+
+import petsWebIcon from '../../res/images/pawprint-black.png';
+import camera from '../../res/images/camera.png';
+import edit from '../../res/images/edit-2.png'
 
 const Rescue = () => {
-    const style = {
-		fontFamily: 'Poppins',
-		fontSize: '2em',
-		position: 'absolute',
-		top: '50%',
-		left: '50%',
-		transform: 'translate(-50%, -50%)'
-	};
-	return (
-		<div className="rescue-module" style={{ height: '50vh' }}>
-			<h2 style={style}>Rescue</h2>
-		</div>
+
+	const [petPic, setPetPic] = useState(null);
+
+    const onChange = e => {
+        // setFile(e.target.files[0]);
+        // setFilename(e.target.files[0].name);
+        setPetPic(URL.createObjectURL(e.target.files[0]));
+    };
+
+    return(
+        <div className="rescue-module">
+            <div className="pet-picture"> 
+                {
+                    petPic ? 
+                        <div className="show-pet-pic">
+                            <img src={petPic} alt=""/>
+                        </div> 
+                        :
+                        <div className="pet-placeholder">
+                            <img src={petsWebIcon} alt="Pet"/>
+                        </div>
+                }
+            </div>
+            <div className="input-wrapper">
+                {
+                    petPic ? 
+                    <img src={edit} alt="" />
+                    :
+                    <img src={camera} alt="" />
+                }
+                <input type="file" capture="camera" accept="image/*" id="cameraInput" onChange={onChange} name="cameraInput"></input>
+            </div>
+        </div>
 	);
 }
 
